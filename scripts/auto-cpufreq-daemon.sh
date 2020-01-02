84e0c6cfa19bf761c1ba85ba8258512adb797072
Adnan Hodzic <adnan@hodzic.org>
2020-01-02 01:49:01 +0100

    added option to deploy and run auto-cpufreq as a service (daemon)

---
+++ scripts/auto-cpufreq-daemon.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#
+# auto-cpufreq daemon deploy script
+# reference: https://github.com/AdnanHodzic/auto-cpufreq
+
+echo -e "\n------------------ Running auto-cpufreq daemon deploy script ------------------"
+
+if [[ $EUID != 0 ]]; 
+then
+	echo -e "\nERROR\nMust be run as root (i.e: 'sudo $0')\n"
+	exit 1
+fi
+
+logs_file="/var/log/auto-cpufreq.log"
+
+echo -e "\n* Reloading systemd manager configuration"
+systemctl daemon-reload
+
+echo -e "\n* Stopping auto-cpufreq systemd service"
+systemctl stop auto-cpufreq
+
+echo -e "\n* Starting auto-cpufreq systemd service"
+systemctl start auto-cpufreq
+
+echo -e "\n* Enabling auto-cpufreq systemd service at boot"
+systemctl enable auto-cpufreq
+
+echo -e "\n* Starting auto-cpufreq systemd service"
+/usr/bin/python3 /usr/bin/auto-cpufreq --live > $logs_file 2>&1 &
+
+echo -e "\n------\n"
