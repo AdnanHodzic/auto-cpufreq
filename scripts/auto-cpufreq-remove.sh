2490de007a10167616a8c05636689a4f174d7ff0
Adnan Hodzic <adnan@hodzic.org>
2020-01-03 14:09:07 +0100

    systemd working with proper logging functionality

--- scripts/auto-cpufreq-remove.sh
+++ scripts/auto-cpufreq-remove.sh
@@ -11,11 +11,42 @@ then
 	exit 1
 fi
 
-echo -e "\n* Disabling auto-cpufreq systemd service at boot"
-systemctl disable auto-cpufreq
-
 echo -e "\n* Stopping auto-cpufreq systemd service"
 systemctl stop auto-cpufreq
 
+echo -e "\n* Disabling auto-cpufreq systemd service at boot"
+systemctl disable auto-cpufreq
+
+#if [ -f /lib/systemd/system/auto-cpufreq.service ];
+#then
+#	rm /lib/systemd/system/auto-cpufreq.service
+#	systemctl daemon-reload
+
+#if [ -f /etc/systemd/system/auto-cpufreq.service ];
+#then
+#	rm /etc/systemd/system/auto-cpufreq.service
+#	systemctl daemon-reload
+
+rm /lib/systemd/system/auto-cpufreq.service
+rm /etc/systemd/system/auto-cpufreq.service
+
 echo -e "\n* Reloading systemd manager configuration"
 systemctl daemon-reload
+
+echo -e "reset failed"
+systemctl reset-failed
+
+echo -e "\n* Removing auto-cpufreq daemon deploy script"
+rm /usr/bin/auto-cpufreq-deploy
+
+echo -e "\n* Removing auto-cpufreq daemon run script"
+rm /usr/bin/auto-cpufreq-run
+
+echo -e "\n* Removing auto-cpufreq binary"
+rm /usr/bin/auto-cpufreq
+
+echo -e "\n* Removing auto-cpufreq log file"
+rm /var/log/auto-cpufreq.log
+
+echo "kill any remaining instances"
+pkill -f auto-cpufreq
