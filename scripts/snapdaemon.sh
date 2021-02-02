02d58f9f2010049bd9bc7c918d5743149a8712ae
Marco Vermeulen <vermeulen.mp@gmail.com>
2021-02-02 20:40:55 +0000

    Improve domain language around logging and stats (#160)
    * Ignore Pycharm descriptors from VCS.
    
    * Rename all occurances of `log` to `stats`.
    
    * Rename log file to stats and move to /var/run
    
    * Bring back --log switch with deprecation message.

--- scripts/snapdaemon.sh
+++ scripts/snapdaemon.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #
 # workaround for running Daemon without polluting syslog (#53, #82)
-$SNAP/bin/auto-cpufreq --daemon 2>&1 >> $SNAP_DATA/auto-cpufreq.log
+$SNAP/bin/auto-cpufreq --daemon 2>&1 >> $SNAP_DATA/auto-cpufreq.stats
