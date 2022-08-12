14167b1c48bb7a7a8d1484fa29664ea3e994bfbe
Anas <AnasR7@protonmail>
2022-08-13 00:29:12 +0700

    add s6 script.

--- scripts/auto-cpufreq-remove.sh
+++ scripts/auto-cpufreq-remove.sh
@@ -61,6 +61,14 @@ elif [ "$(ps h -o comm 1)" = "init" ];then
 
 	echo -e "\n* Removing auto-cpufreq daemon (openrc) unit file"
 	rm /etc/init.d/auto-cpufreq
+# Remove service for s6
+elif [ "$(ps h -o comm 1)" = "s6" ];then
+	echo -e "\n* Stopping auto-cpufreq daemon (s6) service"
+	s6-rc -d change auto-cpufreq default
+	echo -e "\n* Disabling auto-cpufreq daemon (s6) at boot"
+	s6-service delete default auto-cpufreq
+	echo -e "\n* Removing auto-cpufreq daemon (s6) unit file"
+	rm -rf /etc/s6/sv/auto-cpufreq
 else
   echo -e "\n* Unsupported init system detected, could not remove the daemon\n"
   echo -e "\n* Please open an issue on https://github.com/AdnanHodzic/auto-cpufreq\n"
