ad88607013a583a8c8d59ab7732e20786a98f13c
Anas <mywildnest7@gmail.com>
2022-08-13 01:47:17 +0700

    Fix my typo.
    forgot to add slash to this dir.

--- scripts/auto-cpufreq-install.sh
+++ scripts/auto-cpufreq-install.sh
@@ -82,7 +82,7 @@ elif [ "$(ps h -o comm 1)" = "init" ];then
 # Install script for s6
 elif [ "$(ps h -o comm 1)" = "s6-svscan" ];then
 	echo -e "\n* Deploying auto-cpufreq s6 unit file"
-    cp -r /usr/localshare/auto-cpufreq/scripts/auto-cpufreq-s6 /etc/s6/sv/auto-cpufreq
+    cp -r /usr/local/share/auto-cpufreq/scripts/auto-cpufreq-s6 /etc/s6/sv/auto-cpufreq
     echo -e "\n* Update daemon service bundle (s6)"
     s6-db-reload
     echo -e "\n* Add auto-cpufreq service (s6) to default bundle"
