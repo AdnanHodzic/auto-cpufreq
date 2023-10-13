8c613792899ca8171464f67afb8df6db8bff7c7d
isabel <isabel@isabelroses.com>
2023-10-13 15:31:59 +0100

    refactor(nix): reorganise files

--- flake.nix
+++ flake.nix
@@ -1,21 +1,20 @@
 {
-    inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
 
-    outputs = {self, nixpkgs}@inputs :
-    let
-        system = "x86_64-linux"; # replace this as needed
-        pkgs = nixpkgs.legacyPackages.${system};
-        auto-cpufreq = pkgs.python3Packages.callPackage ./nix/default.nix {};
-    in {
-        packages.${system}.default = auto-cpufreq; 
+  outputs = {nixpkgs, ...} @ inputs: let
+    forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux" "x86_64-darwin" "i686-linux" "aarch64-linux" "aarch64-darwin"];
+    pkgsForEach = nixpkgs.legacyPackages;
+  in {
+    packages = forAllSystems (system: {
+      default = pkgsForEach.${system}.python3Packages.callPackage ./nix/default.nix {};
+    });
 
-	    devShells.${system}.default = pkgs.mkShell {
-		    inputsFrom = [ auto-cpufreq ];
-            packages = [ pkgs.python310Packages.pip ];
-	    };
+    devShells = forAllSystems (system: {
+      default = pkgsForEach.${system}.callPackage ./nix/shell.nix {};
+    });
 
-        nixosModules.default = import ./nix/module.nix inputs;
-    };
+    nixosModules.default = import ./nix/module.nix inputs;
+  };
 }
--- nix/module.nix
+++ nix/module.nix
@@ -1,43 +1,46 @@
-inputs: { config, lib, pkgs, options, ... }:
-
+inputs: {
+  config,
+  lib,
+  pkgs,
+  options,
+  ...
+}:
 with lib; let
+  cfg = config.programs.auto-cpufreq;
+  system = "x86_64-linux";
+  defaultPackage = inputs.self.packages.${system}.default;
+  cfgFilename = "auto-cpufreq.conf";
+  cfgFile = format.generate cfgFilename cfg.settings;
 
-    cfg = config.programs.auto-cpufreq;
-    system = "x86_64-linux";
-    defaultPackage = inputs.self.packages.${system}.default;
-    cfgFilename = "auto-cpufreq.conf";
-    cfgFile = format.generate cfgFilename cfg.settings;
-
-    format = pkgs.formats.ini {};
-
+  format = pkgs.formats.ini {};
 in {
+  options.programs.auto-cpufreq = {
+    enable = mkEnableOption "Automatic CPU speed & power optimizer for Linux";
+    #gui.enable = mkEnableOption "Enable GUI";
 
-    options.programs.auto-cpufreq = {
-        enable = mkEnableOption "Automatic CPU speed & power optimizer for Linux";
+    settings = mkOption {
+      description = mdDoc ''
+        Configuration for `auto-cpufreq`.
 
-        settings = mkOption {
-            description = lib.mdDoc ''
-            Configuration for `auto-cpufreq`.
+        See its [example configuration file] for supported settings.
+        [example configuration file]: https://github.com/AdnanHodzic/auto-cpufreq/blob/master/auto-cpufreq.conf-example
+      '';
 
-            See its [example configuration file] for supported settings.
-            [example configuration file]: https://github.com/AdnanHodzic/auto-cpufreq/blob/master/auto-cpufreq.conf-example
-            '';
-
-            default = {};
-            type = types.submodule { freeformType = format.type; };
-        };
+      default = {};
+      type = types.submodule {freeformType = format.type;};
     };
+  };
 
-    config = mkIf cfg.enable {
-        environment.systemPackages = [ defaultPackage ];
+  config = mkIf cfg.enable {
+    environment.systemPackages = [defaultPackage];
 
-        services.auto-cpufreq.enable = true;
-        systemd.services.auto-cpufreq = {
-            overrideStrategy = "asDropin";
-            serviceConfig.ExecStart = lib.mkForce [ 
-                "" 
-                "${defaultPackage}/bin/auto-cpufreq --daemon --config ${cfgFile}"
-            ]; 
-        };
+    services.auto-cpufreq.enable = true;
+    systemd.services.auto-cpufreq = {
+      overrideStrategy = "asDropin";
+      serviceConfig.ExecStart = mkForce [
+        ""
+        "${getBin} ${defaultPackage} --daemon --config ${cfgFile}"
+      ];
     };
+  };
 }
