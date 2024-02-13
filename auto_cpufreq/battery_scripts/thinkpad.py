539914a54502b9ca87b249b23e907590634f2a41
PurpleWazard <155469001+PurpleWazard@users.noreply.github.com>
2024-02-13 12:34:11 -0600

    Rebuilt battery scripts (#645)
    * rebuilt thinkpad and ideapad
    
    * updated conf example

--- auto_cpufreq/battery_scripts/thinkpad.py
+++ auto_cpufreq/battery_scripts/thinkpad.py
@@ -1,49 +1,37 @@
 #!/usr/bin/env python3
 import os
+import subprocess
 from auto_cpufreq.core import root_check
+
+
+def set_battery(value, mode, bat):
+    file_path = f'/sys/class/power_supply/BAT{bat}/charge_{mode}_threshold'
+    try:
+        subprocess.check_output(f"echo {value} | tee /sys/class/power_supply/BAT{bat}/charge_{mode}_threshold", shell=True, text=True)
+    except Exception as e:
+        print(f"Error writing to {file_path}: {e}")
 
 
 def thinkpad_setup(start_threshold, stop_threshold):
     root_check()
-    # this path is specific to thinkpads
-    path_to_bats = '/sys/class/power_supply/'
-    # gets the numb of batteries
-    battery_count = len([name for name in os.listdir(path_to_bats) if name.startswith('BAT')])
-
-    for b in range(battery_count):
-
-        try:
-            with open(f'{path_to_bats}BAT{b}/charge_start_threshold', 'w') as f:
-                f.write(str(start_threshold) + '\n')
-                f.close()
-        except Exception as e:
-            print(f"could not write to BAT{b} start threshold")
-            print(e)
-
-        try:
-            with open(f'{path_to_bats}BAT{b}/charge_stop_threshold', 'w') as f:
-                f.write(str(stop_threshold) + '\n')
-                f.close()
-
-        except Exception as e:
-            print(f"could not write to BAT{b} stop threshold you might be setting it too low try < 65")
-            print(e)
-            pass
+    battery_count = len([name for name in os.listdir("/sys/class/power_supply/") if name.startswith('BAT')])
+    for bat in range(battery_count):
+        set_battery(start_threshold, "start", bat)
+        set_battery(stop_threshold, "stop", bat)
 
 
 def thinkpad_print_thresholds():
     root_check()
-    # this path is specific to thinkpads
-    path_to_bats = '/sys/class/power_supply/'
-    battery_count = len([name for name in os.listdir(path_to_bats) if name.startswith('BAT')])
+    battery_count = len([name for name in os.listdir(
+        "/sys/class/power_supply/") if name.startswith('BAT')])
     print(f"number of batteries = {battery_count}")
     for b in range(battery_count):
         try:
-            with open(f'{path_to_bats}BAT{b}/charge_start_threshold', 'r') as f:
+            with open(f'/sys/class/power_supply/BAT{b}/charge_start_threshold', 'r') as f:
                 print(f'battery{b} start threshold is set to {f.read()}')
                 f.close()
 
-            with open(f'{path_to_bats}BAT{b}/charge_stop_threshold', 'r') as f:
+            with open(f'/sys/class/power_supply/BAT{b}/charge_stop_threshold', 'r') as f:
                 print(f'battery{b} stop threshold is set to {f.read()}')
                 f.close()
 
