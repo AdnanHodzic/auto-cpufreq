00c963080e61d5bd3e25114609f51014854f31ea
shadeyg56 <shadeyg56@gmail.com>
2024-03-18 20:05:59 -0500

    config: update comments

--- auto_cpufreq/utils/config_event_handler.py
+++ auto_cpufreq/utils/config_event_handler.py
@@ -8,20 +8,22 @@ class ConfigEventHandler(pyinotify.ProcessEvent):
         if event.pathname.rstrip("~") == self.config.path:
             self.config.update_config()
 
-    # activates when file is modified
+    # activates when auto-cpufreq config file is modified
     def process_IN_MODIFY(self, event: pyinotify.Event) -> None:
         self._process_update(event)
 
-    # activates when file is deleted
+    # activates when auto-cpufreq config file is deleted
     def process_IN_DELETE_SELF(self, event: pyinotify.Event) -> None:
         self._process_update(event)
 
-    # activates when file is created
+    # activates when auto-cpufreq config file is created
     def process_IN_CREATE(self, event: pyinotify.Event) -> None:
         self._process_update(event)
 
+    # activates when auto-cpufreq config file is moved from watched directory
     def process_IN_MOVED_FROM(self, event: pyinotify.Event) -> None:
         self._process_update(event)
 
+    # activates when auto-cpufreq config file is moved into the watched directory
     def process_IN_MOVED_TO(self, event: pyinotify.Event) -> None:
         self._process_update(event)
