947e69c79d6a696d3b8539083d6649d0354ce86f
Eduardo Portilho <eduardomportilho@gmail.com>
2024-07-13 15:46:58 -0300

    Fix: Getting incorrect CPU Max value

--- scripts/cpufreqctl.sh
+++ scripts/cpufreqctl.sh
@@ -202,12 +202,12 @@ function set_frequency_max () {
 
 function get_frequency_min_limit () {
   if [ -z $CORE ]; then CORE=0; fi
-  cat $FLROOT/cpu$CORE/cpufreq/cpuinfo_min_freq
+  cat $FLROOT/cpu$CORE/cpufreq/scaling_min_freq
 }
 
 function get_frequency_max_limit () {
   if [ -z $CORE ]; then CORE=0; fi
-  cat $FLROOT/cpu$CORE/cpufreq/cpuinfo_max_freq
+  cat $FLROOT/cpu$CORE/cpufreq/scaling_max_freq
 }
 
 function get_energy_performance_preference () {
