38de6aa2bb04ee4e1c7ddc19205d218a953740c5
Casper van Mourik <12167073+CasperVM@users.noreply.github.com>
2025-11-13 19:47:43 +0200

    fix: (WIP) start/stop thresholds not being set because of initial values

--- auto_cpufreq/battery_scripts/thinkpad.py
+++ auto_cpufreq/battery_scripts/thinkpad.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 import os
-from subprocess import check_output
+from subprocess import check_output, CalledProcessError
 
 from auto_cpufreq.config.config import config
 from auto_cpufreq.globals import POWER_SUPPLY_DIR
 
-def set_battery(value, mode, bat):
+def set_battery(value, mode, bat) -> bool:
+    """
+    Return false only if command fails
+    """
     path = f"{POWER_SUPPLY_DIR}{bat}/charge_{mode}_threshold"
-    if os.path.isfile(path): check_output(f"echo {value} | tee {path}", shell=True, text=True)
-    else: print(f"WARNING: {path} does NOT exist")
+    if os.path.isfile(path): 
+        try:
+            check_output(f"echo {value} | tee {path}", shell=True, text=True)
+        except CalledProcessError as e:
+          print(f"WARNING: Could not write battery threshold {value} to {path}")
+          return False
+    else: 
+        print(f"WARNING: {path} does NOT exist")
+    return True
 
 def get_threshold_value(mode):
     conf = config.get_config()
@@ -23,8 +33,15 @@ def thinkpad_setup():
         batteries = [name for name in os.listdir(POWER_SUPPLY_DIR) if name.startswith('BAT')]
 
         for bat in batteries:
-            set_battery(get_threshold_value("start"), "start", bat)
-            set_battery(get_threshold_value("stop"), "stop", bat)
+            could_set_start = set_battery(get_threshold_value("start"), "start", bat)
+            could_set_stop = set_battery(get_threshold_value("stop"), "stop", bat)
+            if not could_set_start or not could_set_stop:
+                # Maybe the values don't line up properly with the currently set ones, try in reverse order
+                # E.g. start is high that the initial stop
+                could_set_stop = set_battery(get_threshold_value("stop"), "stop", bat)
+                could_set_start = set_battery(get_threshold_value("start"), "start", bat)
+            if not could_set_start or not could_set_stop:
+                raise OSError(f"Could not set battery thresholds for {bat}!")
     else: print(f"WARNING {POWER_SUPPLY_DIR} does NOT esixt")
 
 
