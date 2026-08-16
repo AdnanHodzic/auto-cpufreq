2947f76911b587c7da54ec93fcfa5214897a1403
Lucas <122493239+lucasleocs@users.noreply.github.com>
2026-08-16 19:43:24 -0300

    Fix GTK config initialization
    Load the auto-cpufreq configuration before constructing the GTK window so EPP and EPB reporting uses configured values instead of profile fallbacks.
    
    Fixes AdnanHodzic/auto-cpufreq#934

--- auto_cpufreq/bin/auto_cpufreq_gtk.py
+++ auto_cpufreq/bin/auto_cpufreq_gtk.py
@@ -3,9 +3,11 @@ import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk, GLib
 
+from auto_cpufreq.config.config import config, find_config_file
 from auto_cpufreq.gui.app import ToolWindow
 
 def main():
+    config.set_path(find_config_file(None))
     GLib.set_prgname("auto-cpufreq-gtk")
     win = ToolWindow()
     win.connect("destroy", Gtk.main_quit)
